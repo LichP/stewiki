1bccfc6dbc4075e8d39bb9431d39f1aef14ddb6f
Phil Stewart <phil.stewart@lichp.co.uk>
2012-05-12 18:05:47 +0100

    Add user support to Sinatra app

--- lib/stewiki/server.rb
+++ lib/stewiki/server.rb
@@ -2,40 +2,70 @@ require File.dirname(__FILE__) + '/../stewiki'
 require 'sinatra/base'
 
 class Stewiki::Server < Sinatra::Base
+  use Rack::Session::Cookie
+  use Shield::Middleware, "/login"
 
   set :root, File.dirname(__FILE__)  + '/server'
 
-  helpers do
-  end
-
+  helpers Shield::Helpers
+  
   get "/" do
     redirect to('/page/Home')
   end
+  
+  get "/login" do
+    haml :login, :layout => false
+  end
 
+  post "/login" do
+    if login(Stewiki::User, params[:login], params[:password], params[:remember_me])
+      redirect(params[:return] || "/")
+    else
+      redirect "/login"
+    end
+  end
+  
+  get "/logout" do
+    logout(Stewiki::User)
+    redirect "/"
+  end
+              
   get "/page/:pagename/?:commit?" do
+    @user = authenticated(Stewiki::User)
     @title = params[:pagename]
     @page = Stewiki::Page[params[:pagename]]
     @quicklinks = Stewiki::Page["QuickLinks"]
     @page.version = params[:commit] if params[:commit]
+
     haml :display
   end
 
   get "/edit/:pagename" do
+    @user = authenticated(Stewiki::User)
+    error(401) unless @user
+
     @title = "Editing: " + params[:pagename]
     @page = Stewiki::Page[params[:pagename]]
     @quicklinks = Stewiki::Page["QuickLinks"]
+
     haml :edit
   end
 
   post "/edit/:pagename" do
-    Stewiki::Page[params[:pagename]].update(params[:content], params[:commit_message])
+    user = authenticated(Stewiki::User)
+    error(401) unless user
+
+    Stewiki::Page[params[:pagename]].update(params[:content], params[:commit_message], user)
+
     redirect to("/page/" + params[:pagename])
   end
 
   get "/history/:pagename" do
+    @user = authenticated(Stewiki::User)
     @title = "History: " + params[:pagename]
     @page = Stewiki::Page[params[:pagename]]
     @quicklinks = Stewiki::Page["QuickLinks"]
+
     haml :history
   end
 end
